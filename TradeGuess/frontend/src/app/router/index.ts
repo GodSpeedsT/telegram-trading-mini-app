2e4f080b747513598e9fd25a260225ca1c720a55
Nikita <nktvrntsv@gmail.com>
2025-12-13 12:51:31 +0300

    add fsd metodoligua

--- TradeGuess/frontend/src/app/router/index.ts
+++ TradeGuess/frontend/src/app/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { menu } from '@/pages/menu'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'menu',
+      component: menu,
+    },
+  ],
 })
 
 export default router
---
+++ TradeGuess/frontend/src/pages/menu/menu.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+Penis
+</template>
+
+<style scoped>
+
+</style>

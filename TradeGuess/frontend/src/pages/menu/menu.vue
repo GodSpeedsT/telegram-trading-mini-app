86c3fa24c4e79d455fb554f3c5351dd80402bb4f
OstrovskyIv <ostrovskyiml@gmail.com>
2025-12-15 20:09:44 +0300

    Добавлен и настроен tailwindcss + база меню

--- TradeGuess/frontend/src/pages/menu/menu.vue
+++ TradeGuess/frontend/src/pages/menu/menu.vue
@@ -1,4 +1,17 @@
-<script setup>
+<template>
+  <div class="flex flex-col justify-center items-center gap-3 p-2">
+    <div class="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center text-2xl">
+      👤
+    </div>
+    <span class="font-bold text-gray-700">Гость</span>
+    <!-- показ регестриции-->
+
+    <h1 @click="onClick" class="cursor-pointer hover:underline">График нажми на меня</h1>
+    <h1 class="test cursor-pointer hover:underline" @click="onClickTest">График (ТЕСТ) нажми на меня</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
 import router from '@/app/router/index.js'
 
 function onClick() {
@@ -9,11 +22,6 @@ function onClickTest() {
   router.push({ path: '/test' });
 }
 </script>
-
-<template>
-  <h1 @click="onClick">График нажми на меня</h1>
-  <h1 class="test" @click="onClickTest">График (ТЕСТ) нажми на меня</h1>
-</template>
 
 <style scoped>
 .test {
